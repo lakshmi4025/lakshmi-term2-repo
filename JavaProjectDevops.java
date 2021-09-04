e9775bdd18b63c791fb61aea50e6de421989f478
lakshmi4025 <88842239+lakshmi4025@users.noreply.github.com>
2021-09-04 05:23:23 -0700

    Update JavaProjectDevops.java

--- JavaProjectDevops.java
+++ JavaProjectDevops.java
@@ -2,7 +2,7 @@
  class JavaProject {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to the world of GitHub");
+		System.out.println("Welcome to the world of Gitlab");
 
 	}
 
